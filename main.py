0aab39ece75419d1598e944e55fc9714c4a4e8da
aizwellenstan <aizwellenstan@gmail.com>
2021-05-28 01:04:58 +0900

    add read csv

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ ib = IB()
 ib.connect('127.0.0.1', 7497, clientId=5)
 
 try:
-
+  df = pd.read_csv (r'.data/trades.csv', index_col=0)
+  df.drop
+  trades = json.loads(df.to_json(orient = 'records'))
 except Exception as e:
   print("Error on line {}".format(sys.exc_info()[-1].tb_lineno))
   print(e)
